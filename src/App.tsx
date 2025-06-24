ebf34f470b33d1a5c77075602d4afb3da5017394
Traman2 <tejassraman@gmail.com>
2025-06-23 22:41:40 -0500

    fix file path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/Login";
+import LoginPage from "./pages/login";
 import LandingPage from "./pages/LandingPage";
 import SocketLayout from "./pages/Socket";
 
